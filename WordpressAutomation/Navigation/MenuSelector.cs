e1a6cfbfe8dc67651cbbc7fd0a53d5a683ceef42
Boyuan Feng <boyuan.feng@outlook.com>
2017-08-07 11:21:18 -0400

    cannot fix menu bug, leave it for now

--- WordpressAutomation/Navigation/MenuSelector.cs
+++ WordpressAutomation/Navigation/MenuSelector.cs
@@ -13,18 +13,16 @@ namespace WordpressAutomation
             var element = Driver.Instance.FindElement(By.Id(topLevelMenuId));
             Actions moveAction = new Actions(Driver.Instance);
             moveAction.MoveToElement(element).Build().Perform();
-            //Thread.Sleep(1000);
-            //new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10)).Until(
-            //    ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.LinkText(subMenuLinkText)));
-            //    //PresenceOfAllElementsLocatedBy(By.LinkText(subMenuLinkText)));
-            ////ElementToBeClickable(By.LinkText(subMenuLinkText))); 
-            //var subMenubotton = Driver.Instance.FindElement(By.LinkText(subMenuLinkText));
-            //subMenubotton.Click();
 
-            WebDriverWait waitAddNew = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(5));
-            var addNew = waitAddNew.Until(x => x.FindElement(By.LinkText(subMenuLinkText)));
-            moveAction.MoveToElement(addNew).Click();
-            moveAction.Build().Perform();
+            new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10)).Until(
+                ExpectedConditions.ElementToBeClickable(By.LinkText(subMenuLinkText)));
+            var addNew = Driver.Instance.FindElement(By.LinkText(subMenuLinkText));
+
+            //Or We can do:
+            //WebDriverWait waitAddNew = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(5));
+            //var addNew = waitAddNew.Until(x => x.FindElement(By.LinkText(subMenuLinkText)));
+
+            moveAction.MoveToElement(addNew).Click().Build().Perform();
         }
     }
 }
